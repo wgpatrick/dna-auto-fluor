d900aacc2ef598ac0ec7731563831459471269b5
Will Patrick <willpatrick@dhcp-18-111-15-195.dyn.mit.edu>
2014-02-04 17:44:52 -0500

    location matrix done

--- dockingstrands.py
+++ dockingstrands.py
@@ -12,17 +12,31 @@ strands = file_contents["vstrands"]
 
 
 num_strands = len(strands)
-print strands[0]['scaf'][0][0]
+
+
+# Make a location matrix which contains column num, row, column, beginning strand and staple strand information
+location=[]
 for strand in strands:
+	row = strand['row']
+	col = strand['col']
+	num = strand['num']
 	i = 0
+	# Finds first strand 
 	for scaf in strand['scaf']:
 		if scaf[0] != -1:
-			print i
+			beginning_staple = i
 			break
 		else:
 			i += 1
-
-	end_staple= []
-	print str(strand['row']) + ', ' + str(strand['col']) + ', ' + str(len(strand['scaf']))
-
+	j = 0
+	for scaf in reversed(strand['scaf']):
+		
+		if scaf[0] != -1:
+			end_staple = len(strand['scaf']) - j - 1
+			break
+		else:
+			j += 1
+	location.append([num,row,col,beginning_staple,end_staple])
+print location
+	
    
